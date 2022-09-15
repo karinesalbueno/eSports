fbe889bd9a4bf00d6d4462163f757009d14d7cd8
Karine <karine.costa@somagrupo.com.br>
2022-09-15 13:47:18 -0300

    folders mobile, web and framework tailwindcss

---
+++ web/tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.tsx',
+    './index.html'
+  ],
+  theme: {
+    extend: {
+      backgroundImage:{
+        fundo: "url('/Fundo.png')"
+      }
+    },
+  },
+  plugins: [],
+}
