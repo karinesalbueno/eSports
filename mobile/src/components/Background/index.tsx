d69115c20237fa0464c37bc6dd3e14c2a6b93c47
Karine <karine.costa@somagrupo.com.br>
2022-09-18 16:57:20 -0300

    mobile e web

---
+++ mobile/src/components/Background/index.tsx
@@ -0,0 +1,17 @@
+import { ImageBackground } from 'react-native';
+import { styles } from './styles';
+import backgroundImg from '../../assets/background-galaxy.png'
+
+interface Props {
+    children: React.ReactNode;
+}
+
+export function Background({ children }: Props) {
+    return (
+        <ImageBackground
+            defaultSource={backgroundImg}
+            style={styles.container}>
+            {children}
+        </ImageBackground>
+    );
+}
