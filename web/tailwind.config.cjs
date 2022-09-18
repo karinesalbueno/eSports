15e525a04a93f115ac1c94c83a7995ef338e839a
Karine <karine.costa@somagrupo.com.br>
2022-09-18 17:03:18 -0300

    modificações

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -6,8 +6,10 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage:{
-        fundo: "url('/Fundo.png')"
+      backgroundImage: {
+        fundo: "url('/Fundo.png')",
+        'gradients': 'linear-gradient(0deg, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), linear-gradient(89.86deg, #9572FC 23.08%, #43E7AD 33.94%, #E1D55D 44.57%);',
+        'gradient-shadow': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%);'
       }
     },
   },
